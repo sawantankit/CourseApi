b58593ee28401cfa0039224d5d2824b4d4060a4f
Ankit Sharad <Ankit@gmail.com>
2020-04-24 02:00:40 -0400

    Initial commit

---
+++ src/main/java/io/ankit/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,15 @@
+package io.ankit.springbootstarter.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+	
+	//Method Declarations
+	public List<Course> findByTopicId(String topicId);
+
+		
+	
+	
+}
